5dd1f1da9b63d2c337cd105170a9c4ae0655f21d
Aarón Saavedra Lagares <asaavedral@fpcoruna.afundacion.org>
2022-10-04 14:03:18 +0200

    Tarea 6 de Php completada

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,6 +9,10 @@
 					$v_pulgadas = $_POST ["fcantidad"];
 					$v_metros = $v_pulgadas(s) * 0.0254;
 					echo $v_pulgadas."pulgada(s) =".$v_metros." metro(s)";
+				}elseif ($_POST["funidad"] == "yarda"){
+					$v_yarda = $_POST ["fcantidad"];
+					$v_metros = $v_yarda * 0.9144;
+						echo $v_yarda."yarda(s) =".$v_metros." metro(s) ";
 				} else{
 					echo "Unidad no soportada";
 				}
@@ -22,6 +26,8 @@
 
 			<input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 			<label for="pulgada_input">Pulgada(s)</label><br>
+			<input type="radio" id="yarda_input" name="funidad" value="yarda">
+			<label for"yarda_input">Yarda(s)</label><br>
 			<input type="radio" id="otro_input" name="funidad" value="otro">
 			<label for="otro_input">Otro</label><br>
 
