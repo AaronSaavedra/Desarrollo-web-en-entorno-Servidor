f0bd5840ad31f01de29a835e302b2a6d9baf647b
Aarón Saavedra Lagares <asaavedral@fpcoruna.afundacion.org>
2022-10-13 12:18:36 +0200

    php2 Terminado

--- sprint2phpsql/www/mysite/main.php
+++ sprint2phpsql/www/mysite/main.php
@@ -1,4 +1,36 @@
 <?php
 	$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
 ?>
-<h1>Conexión establecida</h1>
+<html>
+	<style>
+		table, th, td{
+			border: 1px solid black;
+		}
+	</style>
+	<body>
+		<h1>Conexión establecida</h1>
+		<table>
+			<tr>
+				<th>Id</th>		
+				<th>Nombre</th>
+				<th>Url_Imagen</th>
+				<th>Autor</th>
+				<th>Genero</th>
+			</tr>
+			<?php
+			// Lanzar un query
+			$query =  'SELECT * FROM tLibros';
+			$result = mysqli_query($db, $query) or die('Query error');
+			while ($row = mysqli_fetch_array($result)) {
+				echo '<td><a href="/detail.php?id='.$row[0].'">'.$row[0].'</a></td>';
+				echo '<tr>';
+				echo '<td>'.$row[1].'</td>';
+				echo '<td><img src="'.$row[2].'" width="100" height="100"></td>';
+				echo '<td>'.$row[3].'</td>';
+				echo '<td>'.$row[4].'</td>';
+				echo '</tr>';
+			}
+			mysqli_close($db);
+			?>
+	</body>
+</html>
