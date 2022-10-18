a63cfd03059875cadf52a55092554ebfb99ff3f1
AaronSaavedra <113438853+AaronSaavedra@users.noreply.github.com>
2022-10-18 14:31:27 +0200

    Tarea 5 Python

--- sprint2python/factorial2.py
+++ sprint2python/factorial2.py
@@ -4,6 +4,3 @@ def func_factorial2(n):
         f = f*n
         n = n-1
     return f
-
-numero=int(input ("Teclea un número: "))
-print(func_factorial2(numero))
--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,3 +1,15 @@
-from factorial import factorial, factorial
-numero=int(input ("Teclea un número: "))
-print(factorial(numero))
+from re import A
+from factorial import factorial
+from factorial2 import func_factorial2
+print('MENU')
+print('a) primer algoritmo')
+print('b) segundo algoritmo')
+op = input('Teclea una opción: ')
+if op == 'a':
+    numero=int(input ("Teclea un número: "))
+    print(factorial(numero))
+elif op == 'b':
+    numero=int(input ("Teclea un número: "))
+    print(func_factorial2(numero))
+else:
+    print('La opción tecleada es inválida.')
